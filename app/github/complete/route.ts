c98ccf37a9556a268a40ee58e6ac9ae9dd8e17ff
zixcx <dhh12450@gmail.com>
2024-08-24 18:58:24 +0900

    Simplified & Duplicate issue fix

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,6 +1,6 @@
-import githubAuth from "@/lib/auth/github-auth";
+import githubAuth from "@/lib/auth/githubAuth";
 import db from "@/lib/db";
-import getSession from "@/lib/session";
+import getSession, { loginSession } from "@/lib/session";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
@@ -13,36 +13,38 @@ export async function GET(request: NextRequest) {
     }
 
     const access_token = await githubAuth.getGithubToken(code);
-    const { id, avatar_url, login } = await githubAuth.getGithubUserData(
-        access_token
-    );
+    const { github_id, avatar_url, github_name } =
+        await githubAuth.getGithubUserData(access_token);
+    const email = await githubAuth.getGithubEmail(access_token);
 
-    const user = await db.user.findUnique({
+    const user = await db.user.findFirst({
         where: {
-            github_id: id + "",
+            OR: [{ github_id }, { email }],
         },
         select: {
             id: true,
         },
     });
     if (user) {
-        const session = await getSession();
-        session.id = user.id;
-        await session.save();
+        await loginSession(user.id);
+        return redirect("/profile");
+    } else {
+        const checkUsername = await db.user.findUnique({
+            where: { username: github_name },
+            select: { id: true },
+        });
+
+        const newUser = await db.user.create({
+            data: {
+                username: checkUsername ? `github_${github_name}` : github_name,
+                github_id,
+                avatar: avatar_url,
+            },
+            select: {
+                id: true,
+            },
+        });
+        await loginSession(newUser.id);
         return redirect("/profile");
     }
-    const newUser = await db.user.create({
-        data: {
-            username: login,
-            github_id: id + "",
-            avatar: avatar_url,
-        },
-        select: {
-            id: true,
-        },
-    });
-    const session = await getSession();
-    session.id = newUser.id;
-    await session.save();
-    return redirect("/profile");
 }
