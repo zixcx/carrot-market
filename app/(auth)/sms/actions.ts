01ba98dd10ea90d45a8a2d7789d5556fa646b316
zixcx <dhh12450@gmail.com>
2024-08-24 21:23:26 +0900

    Changed the structure

--- app/(auth)/sms/actions.ts
+++ app/(auth)/sms/actions.ts
@@ -5,7 +5,8 @@ import validator from "validator";
 import { redirect } from "next/navigation";
 import db from "@/lib/db";
 import crypto from "crypto";
-import { create } from "domain";
+import { loginSession } from "@/lib/session";
+import twilio from "twilio";
 
 const phoneSchema = z
     .string()
@@ -14,7 +15,23 @@ const phoneSchema = z
         (phone) => validator.isMobilePhone(phone, "ko-KR"),
         "잘못된 전화번호 형식입니다."
     );
-const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+const tokenExist = async (token: number) => {
+    const exists = await db.sMSToken.findUnique({
+        where: {
+            token: token.toString(),
+        },
+        select: { id: true },
+    });
+
+    return exists ? true : false;
+};
+
+const tokenSchema = z.coerce
+    .number()
+    .min(100000)
+    .max(999999)
+    .refine(tokenExist, "잘못된 인증번호입니다.");
 
 interface ActionState {
     token: boolean;
@@ -78,17 +95,44 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
                 },
             });
 
+            const client = twilio(
+                process.env.TWILIO_SID,
+                process.env.TWILIO_AUTH_TOKEN
+            );
+
+            await client.messages.create({
+                body: `인증번호 [${token}]`,
+                from: process.env.TWILIO_PHONE_NUMBER!,
+                // to: result.data,
+                to: process.env.MY_PHONE_NUMBER!,
+            });
+
             return { token: true };
         }
     } else {
-        const result = tokenSchema.safeParse(token);
+        const result = await tokenSchema.safeParseAsync(token);
         if (!result.success) {
             return {
                 token: true,
                 error: result.error.flatten(),
             };
         } else {
-            redirect("/");
+            const token = await db.sMSToken.findUnique({
+                where: {
+                    token: result.data.toString(),
+                },
+                select: {
+                    id: true,
+                    userId: true,
+                },
+            });
+            if (token) {
+                loginSession(token!.userId);
+                await db.sMSToken.delete({
+                    where: { id: token.id },
+                });
+            }
+            redirect("/profile");
         }
     }
 }
